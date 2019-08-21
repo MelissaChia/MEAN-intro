9041db8bc838c33a4de2278f9bf6cd78f52cea15
user <user@pop-os.localdomain>
2019-08-21 21:57:56 +0800

    intro to MEAN STACK - up to pg35

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require("http");
+var upperCase = require("upper-case");
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(upperCase("Hello Node!"));
+  })
+  .listen(8080);
+console.log("Server started on localhost:8080; press Ctrl-C to continue....");
